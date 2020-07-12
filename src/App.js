bceacf140434c0b43058e7fbd78c0dd64930cbd3
Joe Averbukh <joeaverbukh@gmail.com>
2020-07-12 13:40:11 -0700

    Update logs url

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const HomePage = ({ updatePage }) => (
       <Icon id="Travels" url="http://joetravels.com" icon="fa-plane" />
       <Icon
         id="Food diary"
-        url="https://nezaj.github.io/web-bitelog/"
+        url="http://joelogs.com"
         icon="fa-spoon"
       />
       <PageIcon
