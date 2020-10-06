d0103c39fb1a041d36cd99233b46812815405963
Joe Averbukh <joeaverbukh@gmail.com>
2020-10-06 15:00:31 -0400

    Update books

--- src/books.js
+++ src/books.js
@@ -2,6 +2,7 @@ const books = `### 2020
 * The Mom Test (Kindle)
 * No Filter: The Inside Story of Instagram (Kindle)
 * Almanack of Naval Ravikant (Kindle)
+* The Company (Kindle)
 
 ### 2019
 * Never Split the Difference (Kindle)
