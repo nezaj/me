0e691ccd032d920a54c177d062e063cf56866f94
Joe Averbukh <joeaverbukh@gmail.com>
2023-04-03 04:45:51 -0400

    Add essay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactTooltip from "react-tooltip";
+import ReactMarkdown from "react-markdown";
 
 import "font-awesome/css/font-awesome.min.css";
 
+import { updateLocation, clearLocation } from "./location";
 import "./App.css";
+import "./tree.css";
 import "./twemoji.css";
+import treePath from "./tree.md";
 
+// Navigation
+// -----------------
+const getLocationPage = (queryString) => {
+  const rawValue = new URLSearchParams(queryString).get("page");
+  console.log(rawValue);
+  return ["home", "essay"].find((x) => x === rawValue) || "home";
+};
+
+// Components
+// -----------------
 const Icon = ({ id, url, icon }) => (
   <a title={id} href={url} target="_blank" rel="noopener noreferrer">
     <i data-tip data-for={id} className={`twa ${icon} icon`}></i>
@@ -18,6 +32,12 @@ const SocialIcon = ({ id, url, icon }) => (
   </a>
 );
 
+const PageIcon = ({ id, onClick, icon }) => (
+  <span title={id} onClick={onClick}>
+    <i data-tip data-for={id} className={`twa ${icon} icon`}></i>
+  </span>
+);
+
 const Tooltip = ({ id, place }) => (
   <div>
     <ReactTooltip id={id} place={place || "top"} effect="solid">
@@ -26,107 +46,171 @@ const Tooltip = ({ id, place }) => (
   </div>
 );
 
-const App = () => (
-  <div className="container">
-    <div className="header">
-      <div className="avatar"></div>
-      <div className="title">Heya, I'm Joe!</div>
+// Pages
+// -----------------
+const Essay = ({ updatePage }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [md, setMd] = useState("");
+  useEffect(() => {
+    fetch(treePath)
+      .then((res) => res.text())
+      .then((md) => {
+        setMd(md);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <div className="tree-essay">
+      {isLoading ? (
+        <div>...</div>
+      ) : (
+        <div>
+          <ReactMarkdown source={md} />
+          <div className="links">
+            <div className="link" onClick={() => updatePage("home")}>
+              Home
+            </div>
+            <a
+              className="link"
+              href="https://growingpainsbook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Growing Pains Book
+            </a>
+          </div>
+        </div>
+      )}
     </div>
-    <div className="home">
-      <div className="subtitle">Three things I tell myself everyday:</div>
-      <div className="principles">
-        <li>Be present</li>
-        <li>Be honest</li>
-        <li>
-          <a
-            class="consistent"
-            href="https://consistent.fit"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Be consistent
-          </a>
-        </li>
+  );
+};
+
+// App
+// -----------------
+const App = () => {
+  const [page, setPage] = useState(getLocationPage(window.location.search));
+
+  const updatePage = (page) => {
+    page === "home" ? clearLocation() : updateLocation("page", page);
+    setPage(page);
+  };
+
+  let Page;
+  switch (page) {
+    case "essay":
+      Page = <Essay updatePage={updatePage} />;
+      break;
+    case "home":
+    default:
+      Page = <Home updatePage={updatePage} />;
+  }
+
+  return <div>{Page}</div>;
+};
+
+const Home = ({ updatePage }) => (
+  <div className="wrapper">
+    <div className="container">
+      <div className="header">
+        <div className="avatar"></div>
+        <div className="title">Heya, I'm Joe!</div>
       </div>
-      <div className="about">This is some stuff I've created on the web</div>
-      <div className="icons-container">
-        {/* Apps */}
-        <Icon id="Instant" url="https://instantdb.com" icon="twa-sparkles" />
-        <Icon id="Books" url="https://zeneca.io/joe" icon="twa-books" />
-        <Icon
-          id="Daily Deal"
-          url="https://dailydeal.email"
-          icon="twa-martial-arts-uniform"
-        />
-        <Icon
-          id="Senior Engineer Jobsearch Course"
-          url="https://jobsearch.dev/"
-          icon="twa-laptop"
-        />
+      <div className="home">
+        <div className="subtitle">Three things I tell myself everyday:</div>
+        <div className="principles">
+          <li>Be present</li>
+          <li>Be honest</li>
+          <li>
+            <a
+              className="consistent"
+              href="https://consistent.fit"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Be consistent
+            </a>
+          </li>
+        </div>
+        <div className="about">This is some stuff I've created on the web</div>
+        <div className="icons-container">
+          {/* Apps */}
+          <Icon id="Instant" url="https://instantdb.com" icon="twa-sparkles" />
+          <Icon id="Books" url="https://zeneca.io/joe" icon="twa-books" />
+          <Icon
+            id="Daily Deal"
+            url="https://dailydeal.email"
+            icon="twa-martial-arts-uniform"
+          />
+          <Icon
+            id="Senior Engineer Jobsearch Course"
+            url="https://jobsearch.dev/"
+            icon="twa-laptop"
+          />
 
-        {/* Manual breakpoint, hacky but works */}
-        <br />
+          {/* Manual breakpoint, hacky but works */}
+          <br />
 
-        {/* Blogs */}
-        <Icon id="Vibes" url="https://joevibes.com" icon="twa-musical-note" />
-        <Icon id="Travels" url="http://joetravels.com" icon="twa-airplane" />
+          {/* Blogs */}
+          <Icon id="Vibes" url="https://joevibes.com" icon="twa-musical-note" />
+          <Icon id="Travels" url="http://joetravels.com" icon="twa-airplane" />
 
-        {/* Books/Courses */}
-        <Icon
-          id="How to Get Into a Programming Bootcamp"
-          url="https://leanpub.com/programmingbootcamp"
-          icon="twa-graduation-cap"
-        />
-        <Icon
-          id="Growing Pains"
-          url="https://growingpainsbook.com"
-          icon="twa-herb"
-        />
+          {/* Books/Courses */}
+          <Icon
+            id="How to Get Into a Programming Bootcamp"
+            url="https://leanpub.com/programmingbootcamp"
+            icon="twa-graduation-cap"
+          />
+          <PageIcon
+            id="A Tale of Two Trees"
+            onClick={() => updatePage("essay")}
+            icon="twa-herb"
+          />
+        </div>
+        <div className="about">And these are my socials</div>
+        <div className="icons-container">
+          {/* Social */}
+          <SocialIcon
+            id="Github"
+            url="https://github.com/nezaj"
+            icon="fa-github"
+          />
+          <SocialIcon
+            id="Instagram"
+            url="https://www.instagram.com/notafraidof138"
+            icon="fa-instagram"
+          />
+          <SocialIcon
+            id="Twitter"
+            url="https://www.twitter.com/joeaverbukh"
+            icon="fa-twitter"
+          />
+          <SocialIcon
+            id="Youtube"
+            url="https://www.youtube.com/channel/UC0QZuLiH9R_Ec9Xdc1jAf4Q"
+            icon="fa-youtube-play"
+          />
+          <SocialIcon
+            id="LinkedIn"
+            url="https://www.linkedin.com/in/joeaverbukh"
+            icon="fa-linkedin-square"
+          />
+        </div>
+        <Tooltip id="Instant" />
+        <Tooltip id="Books" />
+        <Tooltip id="Daily Deal" />
+        <Tooltip id="Senior Engineer Jobsearch Course" />
+
+        <Tooltip id="Travels" />
+        <Tooltip id="Vibes" />
+        <Tooltip id="How to Get Into a Programming Bootcamp" />
+        <Tooltip id="A Tale of Two Trees" />
+
+        <Tooltip id="Github" />
+        <Tooltip id="Instagram" />
+        <Tooltip id="Twitter" />
+        <Tooltip id="Youtube" />
+        <Tooltip id="LinkedIn" />
       </div>
-      <div className="about">And these are my socials</div>
-      <div className="icons-container">
-        {/* Social */}
-        <SocialIcon
-          id="Github"
-          url="https://github.com/nezaj"
-          icon="fa-github"
-        />
-        <SocialIcon
-          id="Instagram"
-          url="https://www.instagram.com/notafraidof138"
-          icon="fa-instagram"
-        />
-        <SocialIcon
-          id="Twitter"
-          url="https://www.twitter.com/joeaverbukh"
-          icon="fa-twitter"
-        />
-        <SocialIcon
-          id="Youtube"
-          url="https://www.youtube.com/channel/UC0QZuLiH9R_Ec9Xdc1jAf4Q"
-          icon="fa-youtube-play"
-        />
-        <SocialIcon
-          id="LinkedIn"
-          url="https://www.linkedin.com/in/joeaverbukh"
-          icon="fa-linkedin-square"
-        />
-      </div>
-      <Tooltip id="Instant" />
-      <Tooltip id="Books" />
-      <Tooltip id="Daily Deal" />
-      <Tooltip id="Senior Engineer Jobsearch Course" />
-
-      <Tooltip id="Travels" />
-      <Tooltip id="Vibes" />
-      <Tooltip id="How to Get Into a Programming Bootcamp" />
-      <Tooltip id="Growing Pains" />
-
-      <Tooltip id="Github" />
-      <Tooltip id="Instagram" />
-      <Tooltip id="Twitter" />
-      <Tooltip id="Youtube" />
-      <Tooltip id="LinkedIn" />
     </div>
   </div>
 );
---
+++ src/tree.md
@@ -0,0 +1,19 @@
+# A Tale of Two Trees
+
+By Joe Averbukh
+
+Two tree saplings grew in a meadow. One a redwood, the other a sequoia. One day the redwood turned to his friend and said "Let's race. I bet I shall reach the moon faster than you." The sequoia laughed, "you fool, that's impossible, no tree can grow that tall." "Well then I will be the first" replied the confident redwood, "and you shall be my witness."
+
+After one thousand years the meadow transformed into a forest. The two saplings grew into majestic trees, towering over everything. Each day the redwood gleefully reached higher and higher while the sequoia struggled to keep up.
+
+Looking up at his friend the sequoia asked, "Surely you must be content already? You are the tallest being in the forest. And from what the birds tell me, you and I are the tallest trees they have ever seen. We are starting to get old, perhaps it is time we end our childish race and settle down into the earth?" The redwood smiled, "I am more than content my friend, for each day I live with purpose and inch closer to my dreams. I cannot settle down yet. I shall reach the moon and you shall be my witness."
+
+One terrible day a violent lightning storm descended on the earth. The tall and strong redwood bore the brunt of it to protect his friend and the rest of the forest. He soon caught on fire, lighting up in a giant blaze. Although his bark was thick enough to withstand the flames, he did not want the fire to spread below. He opened up his trunk and pulled the fire within, containing it completely. He was soon hollowed out and died. Moved by his sacrifice, the sky laid the storm to rest.
+
+With grief and anger the sequoia shouted at the sky "Why! Why would you take away my friend? He was supposed to reach the moon and I was supposed to be his witness!" What would the sequoia do now? He had spent most of his life with the redwood. In the clear night sky, towering above everyone else in the forest, the sequoia felt deeply alone.
+
+The only thing above him was the moon, and he could no longer bear to look at her. For the first time in ages he turned his eyes downwards and sunk towards the earth. He mourned.
+
+The sequoia continued to hunch over. At first it was due to his depression, for there was too much pain associated with growing up. Over time however, he deliberately grew down. He began to notice things he missed while his head was in the clouds. He observed the plants, insects, and animals of the forest. He listened to their struggles, their achievements, and their dreams. Although the sequoia's heart would still ache for his lost friend, the ancient tree understood the redwood's death bestowed him with an earthly wisdom. The sequoia found peace in connecting with the other beings of the forest for his remaining days.
+
+The moon was full and the sky was clear on the sequoia's last night. The great tree, who had been hunched over for centuries now, fell to the earth. He looked up for the first time since the redwood's death. As he stared the sky called upon the wind to lift up the old remnants of the redwood. In his final moments the sequoia watched his old friend disappear towards the moon.
